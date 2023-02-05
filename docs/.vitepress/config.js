685704a92bd944bd753c712266c90c1e34abe056
hxx <hanxx@geovis.com.cn>
2023-02-05 10:17:28 +0800

    feat: 修改

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,5 @@
 export default {
-  title: "vdrag-topo-ui", //站点标题
+  title: "vdrag-topo-ui-docs", //站点标题
   description: "一个vue3组件库", //mate标签description，多用于搜索引擎抓取摘要
   themeConfig: {
     siteTitle: "vdrag-topo-ui",
